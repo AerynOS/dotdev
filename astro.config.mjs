001301d982612bdd17d4d23e09119c27c53f7e0f
Cameron Braid <cameron@drivenow.com.au>
2025-02-26 20:25:38 +1100

    changed from ordering in frontmatter to in astro.config.mjs which makes it much easier to organise

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,102 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aerynos.dev',
-	integrations: [
-		starlight({
+  site: 'https://aerynos.dev',
+  integrations: [
+    starlight({
       logo: {
         dark: '@/images/logo.svg',
         light: '@/images/logo-light-mode.svg',
         replacesTitle: false,
       },
-			title: 'AerynOS Docs',
-			social: {
-				github: 'https://github.com/AerynOS/dotdev',
-			},
+      title: 'AerynOS Docs',
+      social: {
+        github: 'https://github.com/AerynOS/dotdev',
+      },
       customCss: [
         '@/styles/global.css',
       ],
-			sidebar: [
-				{
-					label: 'Users',
-          autogenerate: { directory: 'Users' },
-				},
-				{
-					label: 'Packaging',
-          autogenerate: { directory: 'Packaging' },
-				},
-			],
-		}),
-	],
-});
+      sidebar: [
+        {
+          label: 'Users',
+          items: [
+            { slug: 'users' },
+            {
+              label: 'Getting Started',
+              items: [
+                { slug: 'users/getting-started' },
+                { slug: 'users/getting-started/requirements' },
+                { slug: 'users/getting-started/downloading' },
+                {
+                  slug: 'users/getting-started/creating-the-live-enviroment'
+                },
+                {
+                  slug: 'users/getting-started/booting-the-live-environment'
+                }
+              ]
+            },
+            {
+              label: 'Desktops',
+              items: [
+                { slug: 'users/desktops' },
+                { slug: 'users/desktops/cosmic' },
+                { slug: 'users/desktops/gnome' }
+              ]
+            }
+          ]
+        },
+        {
+          label: 'Packaging',
+          items: [
+            { slug: 'packaging' },
+            {
+              label: 'Workflow',
+              items: [
+                { slug: 'packaging/workflow' },
+                { slug: 'packaging/workflow/updating-an-existing-recipe' },
+                { slug: 'packaging/workflow/creating-a-new-recipe' },
+                { slug: 'packaging/workflow/basic-workflow' },
+                { slug: 'packaging/workflow/prerequisites' }
+              ]
+            },
+            {
+              label: 'Macros',
+              autogenerate: { directory: 'Packaging/Macros' }
+            },
+            {
+              label: 'Recipes',
+              items: [
+                { slug: 'packaging/recipes' },
+                { slug: 'packaging/recipes/overview' },
+                { slug: 'packaging/recipes/upstreams' },
+                { slug: 'packaging/recipes/metadata' },
+                { slug: 'packaging/recipes/build-deps' },
+                { slug: 'packaging/recipes/package-definition' },
+                {
+                  label: 'Triggers',
+                  items: [
+                    { slug: 'packaging/recipes/triggers' },
+                    { slug: 'packaging/recipes/triggers/overview' },
+                    { slug: 'packaging/recipes/triggers/tx-triggers' }
+                  ]
+                },
+                {
+                  label: 'System Accounts',
+                  items: [
+                    { slug: 'packaging/recipes/system-accounts' },
+                    { slug: 'packaging/recipes/system-accounts/groups' },
+                    { slug: 'packaging/recipes/system-accounts/overview' },
+                    { slug: 'packaging/recipes/system-accounts/users' }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
+      ],
+    }),
+  ],
+})
