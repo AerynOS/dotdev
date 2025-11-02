f03d02b3074145e77b45e72b2cf7da8dec82cdea
CookieSource <36531905+CookieSource@users.noreply.github.com>
2025-11-02 19:00:36 +0100

    Show contributor on last update
    This change shows who made the last update on a page and in what commit that change was made.

--- astro.config.mjs
+++ astro.config.mjs
@@ -37,6 +37,9 @@ export default defineConfig({
       customCss: ["@/styles/global.css"],
       editLink: {
         baseUrl: "https://github.com/AerynOS/dotdev/edit/main/",
+      lastUpdated: true,
+      components: {
+        LastUpdated: "./src/components/LastUpdated.astro",
       },
       plugins: [starlightLinksValidator()],
       sidebar: [
